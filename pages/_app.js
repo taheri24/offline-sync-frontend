281dda615bf1e06070fecc2cd252b125f27cf266
Taheri24 <majid.taheri@yandex.com>
2022-02-10 22:21:40 +0330

    code formating

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.scss'
 import DefaultLayout from '../default-layout';
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { SharedQueryClient } from '../shared/query-client';
 import *  as queryFuncs from './_queryFuncs';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-  
+
 function MyApp({ Component, pageProps }) {
     const layout = React.useMemo(() => ({ component: DefaultLayout || Component?.layout }), [Component])
     const onError = React.useCallback(
@@ -14,28 +14,27 @@ function MyApp({ Component, pageProps }) {
             if (err.isAxiosError) {
                 const { response } = err;
                 const { message } = response.data || {};
-                // TODO : mount toast or any alert  component                    
                 if (message)
-                    toast(message,{type:'error'});
+                    toast(message, { type: 'error' });
             }
         }, []);
-const queryClient = React.useMemo(function () {
-    const freshQueryClient = new SharedQueryClient({
-        queryFuncs,
-        defaultOptions: {
-            queries: {onError},
-            mutations:{onError}
-        },
-    });
-    return freshQueryClient;
-}, [])
+    const queryClient = React.useMemo(function () {
+        const freshQueryClient = new SharedQueryClient({
+            queryFuncs,
+            defaultOptions: {
+                queries: { onError },
+                mutations: { onError }
+            },
+        });
+        return freshQueryClient;
+    }, [])
 
 
-return <QueryClientProvider client={queryClient} > <layout.component>
-    <Component {...pageProps} />
-    <ToastContainer/>
-</layout.component>
-</QueryClientProvider>
+    return <QueryClientProvider client={queryClient} > <layout.component>
+        <Component {...pageProps} />
+        <ToastContainer />
+    </layout.component>
+    </QueryClientProvider>
 }
 
 export default MyApp
--- pages/api/current-device.js
+++ pages/api/current-device.js
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(400).json({  
-        message:`fake Error for UI testing`,
+    
+    // res.status(400).json({   message:`fake Error for UI testing`})
+res.status(200).json({  
+        deviceId:`1`,
     } )
   }
   
