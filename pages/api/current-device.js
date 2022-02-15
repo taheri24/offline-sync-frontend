5115b25f6303e1f4603f610b80c35d66312ef979
Taheri24 <majid.taheri@yandex.com>
2022-02-15 18:06:02 +0330

    update api

--- pages/api/current-device.js
+++ pages/api/current-device.js
@@ -3,6 +3,10 @@
 export default function handler(req, res) {
     res.status(200).json({  
         deviceId:`1`,
+        computerName:'C1',
+        networkIPs:[
+            'sdf'
+        ]
     } )
 }
   
