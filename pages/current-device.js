f40eddb891811a38cfb91e63d66bcdb6165eefac
Taheri24 <majid.taheri@yandex.com>
2022-02-15 18:05:15 +0330

    complete current device v0

--- pages/current-device.js
+++ pages/current-device.js
@@ -1,15 +1,23 @@
 import axios from "axios"
 import { useQuery } from "react-query"
-import { JsonView } from "../components";
 export default function CurrentDevice() {
     const query = useQuery('loadCurrentDevice');
     const {data} = query;
     return <>
-            <JsonView query={query} />  
+               
         <div className="readOnlyField">
             <h3 className="label">DeviceId</h3>
             <input type="text" value={data?.deviceId} />
         </div>
+        <div className="readOnlyField">
+            <h3 className="label">ComputerName</h3>
+            <input type="text" value={data?.computerName} />
+        </div>
+        <div className="readOnlyField" style={{flexDirection: 'column'}}>
+            <h3 className="label">Network IPs</h3>
+           {data?.networkIPs?.map(networkIP=>(   <input type="text" value={networkIP} />))}    
+        </div>
+
     </>
 
 }
